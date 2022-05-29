ed16b0106c80907a52e9bb6cc45a30b7ff6cc66c
Royman <royman.dev@gmail.com>
2022-05-30 05:40:12 +0700

    feat: add PanelPlaylist sub header

--- src/components/PanelPlaylist.tsx
+++ src/components/PanelPlaylist.tsx
@@ -1,11 +1,18 @@
+import { useAtomValue } from 'jotai';
+import { playlistNameAtom } from '../stores/playlistStore';
 import BasePanel from './BasePanel';
 import BasePanelHeader from './BasePanelHeader';
 
 const PanelPlaylist = () => {
+  const playlistName = useAtomValue(playlistNameAtom);
+
   return (
     <BasePanel>
       <BasePanelHeader>
         <h2 className="px-2">Playlist</h2>
+      </BasePanelHeader>
+      <BasePanelHeader sub>
+        <h3 className="px-2">{playlistName}</h3>
       </BasePanelHeader>
     </BasePanel>
   );

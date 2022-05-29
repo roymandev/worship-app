ac5daafb54a29dc537081070d0116424e84efb50
Royman <royman.dev@gmail.com>
2022-05-30 04:57:07 +0700

    feat: implement react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 };
 
 export default App;

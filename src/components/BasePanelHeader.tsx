60d320071c41398e2f7ac6118be75fdcdf2fe52a
Royman <royman.dev@gmail.com>
2022-05-30 05:39:28 +0700

    feat: add sub prop to BasePanelHeader component

--- src/components/BasePanelHeader.tsx
+++ src/components/BasePanelHeader.tsx
@@ -1,6 +1,18 @@
-const BasePanelHeader = ({ children }: { children: React.ReactNode }) => {
+import { twMerge } from 'tailwind-merge';
+
+interface BasePanelHeaderProps {
+  children: React.ReactNode;
+  sub?: boolean;
+}
+
+const BasePanelHeader = ({ children, sub = false }: BasePanelHeaderProps) => {
   return (
-    <div className="flex items-center h-10 border-b border-gray-300">
+    <div
+      className={twMerge(
+        'flex items-center border-b border-gray-300',
+        sub ? 'h-8 bg-gray-100' : 'h-10',
+      )}
+    >
       {children}
     </div>
   );

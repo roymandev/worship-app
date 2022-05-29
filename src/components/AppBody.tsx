67bc11bbf9c02d31c468685231a47821abe9bc39
Royman <royman.dev@gmail.com>
2022-05-30 05:27:59 +0700

    feat: add PanelPlaylist component

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -1,10 +1,11 @@
 import Split from 'react-split';
 import BasePanel from './BasePanel';
+import PanelPlaylist from './PanelPlaylist';
 
 const AppBody = () => {
   return (
     <Split className="flex overflow-hidden flex-1" gutterSize={3}>
-      <BasePanel>Playlist</BasePanel>
+      <PanelPlaylist />
       <BasePanel>Preview</BasePanel>
       <BasePanel>Live</BasePanel>
     </Split>
---
+++ src/components/PanelPlaylist.tsx
@@ -0,0 +1,14 @@
+import BasePanel from './BasePanel';
+import BasePanelHeader from './BasePanelHeader';
+
+const PanelPlaylist = () => {
+  return (
+    <BasePanel>
+      <BasePanelHeader>
+        <h2 className="px-2">Playlist</h2>
+      </BasePanelHeader>
+    </BasePanel>
+  );
+};
+
+export default PanelPlaylist;

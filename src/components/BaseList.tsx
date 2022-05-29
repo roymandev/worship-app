b5336a25a0ccfdaf76437fbad9207f6a1e7f5f3e
Royman <royman.dev@gmail.com>
2022-05-30 06:02:25 +0700

    feat: add BaseList component

---
+++ src/components/BaseList.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { scrollToSelected } from '../lib/scrollToSelected';
+
+export interface BaseListProps<T> extends React.ComponentPropsWithoutRef<'ul'> {
+  items: T[];
+  renderItem: (item: T, index: number) => React.ReactNode;
+  selectedItemIndex?: number;
+  onKeyDown?: React.KeyboardEventHandler<HTMLUListElement>;
+}
+
+export const BaseList = <T,>({
+  items,
+  renderItem,
+  selectedItemIndex,
+  className,
+  onKeyDown,
+  ...rest
+}: BaseListProps<T>) => {
+  const containerRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    if (containerRef.current && selectedItemIndex !== undefined) {
+      scrollToSelected(containerRef.current, selectedItemIndex);
+    }
+  }, [selectedItemIndex]);
+
+  return (
+    <ul
+      ref={containerRef}
+      className={twMerge(className, 'relative')}
+      tabIndex={onKeyDown && 0}
+      onKeyDown={onKeyDown}
+      {...rest}
+    >
+      {items.map(renderItem)}
+    </ul>
+  );
+};
+
+export default BaseList;

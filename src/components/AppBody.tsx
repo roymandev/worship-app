13fe477959230c790c12364c1566fa0297d83462
Royman <royman.dev@gmail.com>
2022-06-01 01:43:22 +0700

    feat: implement TextScreen to PanelPreview

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -1,13 +1,25 @@
+import { useRef } from 'react';
 import Split from 'react-split';
 import BasePanel from './BasePanel';
 import PanelPlaylist from './PanelPlaylist';
 import PanelPreview from './PanelPreview';
+import { TextScreenRef } from './TextScreen';
 
 const AppBody = () => {
+  const panelPreviewTextScreenRef = useRef<TextScreenRef | null>(null);
+
+  const onDragHandler = () => {
+    panelPreviewTextScreenRef.current?.scaleScreen();
+  };
+
   return (
-    <Split className="flex overflow-hidden flex-1 p-1" gutterSize={4}>
+    <Split
+      className="flex overflow-hidden flex-1 p-1"
+      gutterSize={4}
+      onDrag={onDragHandler}
+    >
       <PanelPlaylist />
-      <PanelPreview />
+      <PanelPreview ref={panelPreviewTextScreenRef} />
       <Split direction="vertical" gutterSize={4}>
         <BasePanel>Live Content</BasePanel>
         <BasePanel>Live Screen</BasePanel>
--- src/components/PanelPreview.tsx
+++ src/components/PanelPreview.tsx
@@ -1,12 +1,20 @@
 import { useAtomValue } from 'jotai';
-import { useEffect, useState } from 'react';
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
+import Split from 'react-split';
 import { playlistSelectedItemAtom } from '../stores/playlistStore';
 import BaseList from './BaseList';
 import BasePanel from './BasePanel';
 import BasePanelHeader from './BasePanelHeader';
 import ItemContentLine from './ItemContentLine';
+import TextScreen, { TextScreenRef } from './TextScreen';
 
-const PanelPreview = () => {
+const PanelPreview = forwardRef<TextScreenRef>((props, ref) => {
   const playlistSelectedItem = useAtomValue(playlistSelectedItemAtom);
   const [selectedLineIndex, setSelectedLineIndex] = useState(-1);
 
@@ -14,29 +22,50 @@ const PanelPreview = () => {
     setSelectedLineIndex(playlistSelectedItem?.content[0] ? 0 : -1);
   }, [playlistSelectedItem]);
 
+  const textScreenRef = useRef<TextScreenRef | null>(null);
+
+  const onDragHandler = () => {
+    if (textScreenRef.current) {
+      textScreenRef.current.scaleScreen();
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    scaleScreen: onDragHandler,
+  }));
+
   return (
-    <BasePanel>
-      <BasePanelHeader>
-        <h2 className="px-2">Preview</h2>
-      </BasePanelHeader>
-      <BasePanelHeader sub>
-        <h2 className="px-2">{playlistSelectedItem?.title}</h2>
-      </BasePanelHeader>
-      <BaseList
-        className="leading-4 whitespace-pre-line"
-        items={playlistSelectedItem?.content ?? []}
-        scrollToIndex={selectedLineIndex}
-        renderItem={(line, index) => (
-          <ItemContentLine
-            key={index}
-            line={line}
-            isSelected={index === selectedLineIndex}
-            onClick={() => setSelectedLineIndex(index)}
-          />
-        )}
-      />
-    </BasePanel>
+    <Split direction="vertical" gutterSize={4} onDrag={onDragHandler}>
+      <BasePanel>
+        <BasePanelHeader>
+          <h2 className="px-2">Preview</h2>
+        </BasePanelHeader>
+        <BasePanelHeader sub>
+          <h2 className="px-2">{playlistSelectedItem?.title}</h2>
+        </BasePanelHeader>
+        <BaseList
+          className="leading-4 whitespace-pre-line"
+          items={playlistSelectedItem?.content ?? []}
+          scrollToIndex={selectedLineIndex}
+          renderItem={(line, index) => (
+            <ItemContentLine
+              key={index}
+              line={line}
+              isSelected={index === selectedLineIndex}
+              onClick={() => setSelectedLineIndex(index)}
+            />
+          )}
+        />
+      </BasePanel>
+      <BasePanel>
+        <TextScreen
+          ref={textScreenRef}
+          line={playlistSelectedItem?.content[selectedLineIndex]}
+        />
+      </BasePanel>
+    </Split>
   );
-};
+});
+PanelPreview.displayName = 'PanelPreview';
 
 export default PanelPreview;

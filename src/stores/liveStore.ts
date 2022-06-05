521a6cf8531fe24ba776b09617eb018880c0659c
Royman <royman.dev@gmail.com>
2022-06-05 19:41:08 +0700

    feat(store): add liveStore selected line getter

--- src/stores/liveStore.ts
+++ src/stores/liveStore.ts
@@ -1,3 +1,4 @@
+import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 import type { BaseItem } from '../types/playlistTypes';
 
@@ -6,4 +7,10 @@ export const liveItemAtom = atomWithStorage<BaseItem | null>('liveItem', null);
 export const liveItemSelectedLineIndexAtom = atomWithStorage<number>(
   'liveItemSelectedLineIndex',
   -1,
+);
+
+// Getter
+export const liveItemSelectedLineAtom = atom(
+  (get) =>
+    get(liveItemAtom)?.content[get(liveItemSelectedLineIndexAtom)] ?? null,
 );

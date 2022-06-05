fad4dfbf72e920595f6dd954fc76e12cd816403b
Royman <royman.dev@gmail.com>
2022-06-05 19:42:36 +0700

    feat: PanelLive add list controller

--- src/components/PanelLive.tsx
+++ src/components/PanelLive.tsx
@@ -1,10 +1,17 @@
-import { useAtom, useAtomValue } from 'jotai';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import Split from 'react-split';
+import { listController } from '../lib/listController';
 import {
   liveItemAtom,
+  liveItemSelectedLineAtom,
   liveItemSelectedLineIndexAtom,
 } from '../stores/liveStore';
+import {
+  playlistShiftSelectedItemDownAtom,
+  playlistShiftSelectedItemUpAtom,
+} from '../stores/playlistStore';
+import { BaseItemContentLine } from '../types/playlistTypes';
 import BaseList from './BaseList';
 import BasePanel from './BasePanel';
 import BasePanelHeader from './BasePanelHeader';
@@ -13,21 +20,45 @@ import TextScreen, { TextScreenRef } from './TextScreen';
 
 const PanelLive = forwardRef<TextScreenRef>((props, ref) => {
   const liveItem = useAtomValue(liveItemAtom);
+  const liveItemSelectedLine = useAtomValue(liveItemSelectedLineAtom);
   const [liveItemSelectedLineIndex, setLiveItemSelectedLineIndex] = useAtom(
     liveItemSelectedLineIndexAtom,
   );
 
-  const textScreenRef = useRef<TextScreenRef | null>(null);
+  const contentHandler = listController({
+    items: liveItem?.content ?? [],
+    selectedItemIndex: liveItemSelectedLineIndex,
+    setSelectedItemIndex: (index) => setLiveItemSelectedLineIndex(index),
+  });
 
+  // playlistStore handler
+  const playlistShiftSelectedItemUp = useSetAtom(
+    playlistShiftSelectedItemUpAtom,
+  );
+  const playlistShiftSelectedItemDown = useSetAtom(
+    playlistShiftSelectedItemDownAtom,
+  );
+
+  // Pass scaleScreen method to parent
+  const textScreenRef = useRef<TextScreenRef | null>(null);
   const onDragHandler = () => {
     if (textScreenRef.current) {
       textScreenRef.current.scaleScreen();
     }
   };
-
   useImperativeHandle(ref, () => ({
     scaleScreen: onDragHandler,
   }));
+
+  // Render live item content line
+  const renderContentLine = (line: BaseItemContentLine, index: number) => (
+    <ItemContentLine
+      key={index}
+      line={line}
+      isSelected={index === liveItemSelectedLineIndex}
+      onClick={() => setLiveItemSelectedLineIndex(index)}
+    />
+  );
 
   return (
     <Split direction="vertical" gutterSize={4} onDrag={onDragHandler}>
@@ -35,28 +66,25 @@ const PanelLive = forwardRef<TextScreenRef>((props, ref) => {
         <BasePanelHeader>
           <h2 className="px-2">Preview</h2>
         </BasePanelHeader>
+
         <BasePanelHeader sub>
           <h2 className="px-2">{liveItem?.title}</h2>
         </BasePanelHeader>
+
         <BaseList
           className="leading-4 whitespace-pre-line"
           items={liveItem?.content ?? []}
           scrollToIndex={liveItemSelectedLineIndex}
-          renderItem={(line, index) => (
-            <ItemContentLine
-              key={index}
-              line={line}
-              isSelected={index === liveItemSelectedLineIndex}
-              onClick={() => setLiveItemSelectedLineIndex(index)}
-            />
-          )}
+          onKeyDownArrowUp={contentHandler.shiftSelectedItemUp}
+          onKeyDownArrowDown={contentHandler.shiftSelectedItemDown}
+          onKeyDownArrowLeft={playlistShiftSelectedItemUp}
+          onKeyDownArrowRight={playlistShiftSelectedItemDown}
+          renderItem={renderContentLine}
         />
       </BasePanel>
+
       <BasePanel>
-        <TextScreen
-          ref={textScreenRef}
-          line={liveItem?.content[liveItemSelectedLineIndex]}
-        />
+        <TextScreen ref={textScreenRef} line={liveItemSelectedLine} />
       </BasePanel>
     </Split>
   );

6b70a43cf09ed6789d922a1af62e12540cbb2805
Royman <royman.dev@gmail.com>
2022-06-06 06:39:45 +0700

    feat: implement body tabs in PanelPlaylist

--- src/components/PanelPlaylist.tsx
+++ src/components/PanelPlaylist.tsx
@@ -1,120 +1,40 @@
-import { useAtom, useAtomValue, useSetAtom } from 'jotai';
-import {
-  RiArrowDownLine,
-  RiArrowUpLine,
-  RiDeleteBin2Line,
-} from 'react-icons/ri';
-import {
-  atomPlaylistItems,
-  atomPlaylistName,
-  atomPlaylistSelectedItem,
-  atomPlaylistSelectedItemIndex,
-} from '../stores/playlistStore';
-import {
-  atomLiveItem,
-  atomLiveItemContentSelectedLineIndex,
-} from '../stores/liveStore';
-import { BaseItem, PlaylistItem } from '../types/playlistTypes';
-import BaseInput from './BaseInput';
-import BaseList from './BaseList';
-import BaseListLine from './BaseListLine';
+import { RiCloseFill, RiUploadLine } from 'react-icons/ri';
+
 import BasePanel from './BasePanel';
 import BasePanelHeader from './BasePanelHeader';
 import BaseButton from './BaseButton';
-import { listController } from '../lib/listController';
+import { useState } from 'react';
+import PanelPlaylistList from './PanelPlaylistList';
+import PanelPlaylistImport from './PanelPlaylistImport';
+import { twMerge } from 'tailwind-merge';
 
 const PanelPlaylist = () => {
-  const [playlistName, setPlaylistName] = useAtom(atomPlaylistName);
-  const [playlistItems, setPlaylistItems] = useAtom(atomPlaylistItems);
-  const [playlistSelectedItemIndex, setPlaylistSelectedItemIndex] = useAtom(
-    atomPlaylistSelectedItemIndex,
-  );
-  const playlistSelectedItem = useAtomValue(atomPlaylistSelectedItem);
-
-  const listHandler = listController({
-    items: playlistItems,
-    selectedItemIndex: playlistSelectedItemIndex,
-    setItems: (items) => setPlaylistItems(items),
-    setSelectedItemIndex: (index) => setPlaylistSelectedItemIndex(index),
-  });
-
-  // liveStore
-  const setLiveItem = useSetAtom(atomLiveItem);
-  const setLiveItemSelectedLineIndex = useSetAtom(
-    atomLiveItemContentSelectedLineIndex,
-  );
-  const setLiveItemHandler = (item: BaseItem | null) => {
-    setLiveItem(item);
-    setLiveItemSelectedLineIndex(item?.content[0] ? 0 : -1);
-  };
-
-  // Render playlist item
-  const renderPlaylistItem = (item: PlaylistItem, index: number) => (
-    <BaseListLine
-      className="py-1 px-2 select-none"
-      key={item.id}
-      isSelected={index === playlistSelectedItemIndex}
-      onClick={() => setPlaylistSelectedItemIndex(index)}
-      onDoubleClick={() => setLiveItemHandler(item)}
-    >
-      <h3 className="font-medium">{item.title}</h3>
-      {item.note && <p className="text-slate-600">{item.note}</p>}
-    </BaseListLine>
-  );
+  const [panelBody, setPanelBody] = useState<'list' | 'import'>('list');
 
   return (
     <BasePanel>
       <BasePanelHeader>
         <h2 className="px-2">Playlist</h2>
+        <BaseButton
+          variant="default"
+          className={twMerge(
+            'flex gap-1 items-center pr-3 pl-2 mx-1 ml-auto h-7',
+            panelBody === 'import' && 'border-slate-400 bg-slate-300',
+          )}
+          onClick={() =>
+            setPanelBody(panelBody === 'import' ? 'list' : 'import')
+          }
+        >
+          {panelBody === 'import' ? (
+            <RiCloseFill className="w-4 h-4" />
+          ) : (
+            <RiUploadLine className="w-4 h-4" />
+          )}
+          Import
+        </BaseButton>
       </BasePanelHeader>
-
-      <BasePanelHeader sub>
-        <BaseInput
-          className="flex-1 px-1 mx-1 h-7"
-          value={playlistName}
-          onChange={(e) => setPlaylistName(e.target.value)}
-        />
-      </BasePanelHeader>
-
-      <div className="flex flex-1 divide-x divide-slate-300">
-        <BaseList
-          className="overflow-y-auto flex-1"
-          items={playlistItems}
-          scrollToIndex={playlistSelectedItemIndex}
-          onKeyDownArrowUp={listHandler.shiftSelectedItemUp}
-          onKeyDownArrowDown={listHandler.shiftSelectedItemDown}
-          onKeyDownEnter={() => setLiveItemHandler(playlistSelectedItem)}
-          renderItem={renderPlaylistItem}
-        />
-
-        <div className="flex flex-col gap-1 p-1">
-          <BaseButton
-            variant="default"
-            className="p-1"
-            onClick={listHandler.moveSelectedItemUp}
-            disabled={!listHandler.canShiftSelectedItemUp()}
-          >
-            <RiArrowUpLine className="w-4 h-4" />
-          </BaseButton>
-          <BaseButton
-            variant="default"
-            className="p-1"
-            onClick={listHandler.moveSelectedItemDown}
-            disabled={!listHandler.canShiftSelectedItemDown()}
-          >
-            <RiArrowDownLine className="w-4 h-4" />
-          </BaseButton>
-          <hr />
-          <BaseButton
-            variant="default"
-            className="p-1"
-            onClick={listHandler.removeSelectedItem}
-            disabled={!listHandler.selectedItem()}
-          >
-            <RiDeleteBin2Line className="w-4 h-4" />
-          </BaseButton>
-        </div>
-      </div>
+      {panelBody === 'list' && <PanelPlaylistList />}
+      {panelBody === 'import' && <PanelPlaylistImport />}
     </BasePanel>
   );
 };

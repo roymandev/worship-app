8186d80d78bb5372d60f8d517fdecc783d7d3081
Royman <royman.dev@gmail.com>
2022-06-05 19:35:49 +0700

    feat: PanelPlaylist set list controller

--- src/components/PanelPlaylist.tsx
+++ src/components/PanelPlaylist.tsx
@@ -1,14 +1,17 @@
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import {
-  liveItemAtom,
-  liveItemSelectedLineIndexAtom,
-} from '../stores/liveStore';
-import {
   playlistItemsAtom,
   playlistNameAtom,
   playlistSelectedItemAtom,
+  playlistSelectedItemIndexAtom,
+  playlistShiftSelectedItemDownAtom,
+  playlistShiftSelectedItemUpAtom,
 } from '../stores/playlistStore';
-import { BaseItem } from '../types/playlistTypes';
+import {
+  liveItemAtom,
+  liveItemSelectedLineIndexAtom,
+} from '../stores/liveStore';
+import { BaseItem, PlaylistItem } from '../types/playlistTypes';
 import BaseInput from './BaseInput';
 import BaseList from './BaseList';
 import BaseListLine from './BaseListLine';
@@ -18,24 +21,49 @@ import BasePanelHeader from './BasePanelHeader';
 const PanelPlaylist = () => {
   const [playlistName, setPlaylistName] = useAtom(playlistNameAtom);
   const playlistItems = useAtomValue(playlistItemsAtom);
-  const [playlistSelectedItem, setPlaylistSelectedAtom] = useAtom(
-    playlistSelectedItemAtom,
+  const [playlistSelectedItemIndex, setPlaylistSelectedItemIndex] = useAtom(
+    playlistSelectedItemIndexAtom,
   );
+  const playlistSelectedItem = useAtomValue(playlistSelectedItemAtom);
+
+  // liveStore
   const setLiveItem = useSetAtom(liveItemAtom);
   const setLiveItemSelectedLineIndex = useSetAtom(
     liveItemSelectedLineIndexAtom,
   );
-
-  const setLiveItemHandler = (item: BaseItem) => {
+  const setLiveItemHandler = (item: BaseItem | null) => {
     setLiveItem(item);
-    setLiveItemSelectedLineIndex(item.content[0] ? 0 : -1);
+    setLiveItemSelectedLineIndex(item?.content[0] ? 0 : -1);
   };
+
+  // playlistStore actions
+  const playlistShiftSelectedItemUp = useSetAtom(
+    playlistShiftSelectedItemUpAtom,
+  );
+  const playlistShiftSelectedItemDown = useSetAtom(
+    playlistShiftSelectedItemDownAtom,
+  );
+
+  // Render playlist item
+  const renderPlaylistItem = (item: PlaylistItem, index: number) => (
+    <BaseListLine
+      className="py-1 px-2 select-none"
+      key={item.id}
+      isSelected={index === playlistSelectedItemIndex}
+      onClick={() => setPlaylistSelectedItemIndex(index)}
+      onDoubleClick={() => setLiveItemHandler(item)}
+    >
+      <h3 className="font-medium">{item.title}</h3>
+      {item.note && <p className="text-slate-600">{item.note}</p>}
+    </BaseListLine>
+  );
 
   return (
     <BasePanel>
       <BasePanelHeader>
         <h2 className="px-2">Playlist</h2>
       </BasePanelHeader>
+
       <BasePanelHeader sub>
         <BaseInput
           className="flex-1 px-1 mx-1 h-7"
@@ -43,24 +71,15 @@ const PanelPlaylist = () => {
           onChange={(e) => setPlaylistName(e.target.value)}
         />
       </BasePanelHeader>
+
       <BaseList
         className="overflow-y-auto"
         items={playlistItems}
-        scrollToIndex={playlistItems.findIndex(
-          (item) => item.id === playlistSelectedItem?.id,
-        )}
-        renderItem={(item) => (
-          <BaseListLine
-            className="py-1 px-2 select-none"
-            key={item.id}
-            isSelected={item.id === playlistSelectedItem?.id}
-            onClick={() => setPlaylistSelectedAtom(item)}
-            onDoubleClick={() => setLiveItemHandler(item)}
-          >
-            <h3 className="font-medium">{item.title}</h3>
-            {item.note && <p className="text-slate-600">{item.note}</p>}
-          </BaseListLine>
-        )}
+        scrollToIndex={playlistSelectedItemIndex}
+        onKeyDownArrowUp={playlistShiftSelectedItemUp}
+        onKeyDownArrowDown={playlistShiftSelectedItemDown}
+        onKeyDownEnter={() => setLiveItemHandler(playlistSelectedItem)}
+        renderItem={renderPlaylistItem}
       />
     </BasePanel>
   );

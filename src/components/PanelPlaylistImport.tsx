18fec87317587f7d4eb98148a6ac9f1739abdf16
Royman <royman.dev@gmail.com>
2022-06-06 06:37:36 +0700

    feat: add PanelPlaylistImport component

---
+++ src/components/PanelPlaylistImport.tsx
@@ -0,0 +1,13 @@
+import BasePanelHeader from './BasePanelHeader';
+
+const PanelPlaylistImport = () => {
+  return (
+    <>
+      <BasePanelHeader sub>
+        <h2 className="px-2">Import</h2>
+      </BasePanelHeader>
+    </>
+  );
+};
+
+export default PanelPlaylistImport;

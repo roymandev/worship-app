69bf3fe6aaaf6ac93209fc80923ae4c9492a593c
Royman <royman.dev@gmail.com>
2022-06-06 05:06:13 +0700

    feat: add PanelPlaylist list controllers

--- src/components/PanelPlaylist.tsx
+++ src/components/PanelPlaylist.tsx
@@ -1,11 +1,14 @@
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import {
+  RiArrowDownLine,
+  RiArrowUpLine,
+  RiDeleteBin2Line,
+} from 'react-icons/ri';
+import {
   atomPlaylistItems,
   atomPlaylistName,
   atomPlaylistSelectedItem,
   atomPlaylistSelectedItemIndex,
-  atomPlaylistShiftSelectedItemDown,
-  atomPlaylistShiftSelectedItemUp,
 } from '../stores/playlistStore';
 import {
   atomLiveItem,
@@ -17,14 +20,23 @@ import BaseList from './BaseList';
 import BaseListLine from './BaseListLine';
 import BasePanel from './BasePanel';
 import BasePanelHeader from './BasePanelHeader';
+import BaseButton from './BaseButton';
+import { listController } from '../lib/listController';
 
 const PanelPlaylist = () => {
   const [playlistName, setPlaylistName] = useAtom(atomPlaylistName);
-  const playlistItems = useAtomValue(atomPlaylistItems);
+  const [playlistItems, setPlaylistItems] = useAtom(atomPlaylistItems);
   const [playlistSelectedItemIndex, setPlaylistSelectedItemIndex] = useAtom(
     atomPlaylistSelectedItemIndex,
   );
   const playlistSelectedItem = useAtomValue(atomPlaylistSelectedItem);
+
+  const listHandler = listController({
+    items: playlistItems,
+    selectedItemIndex: playlistSelectedItemIndex,
+    setItems: (items) => setPlaylistItems(items),
+    setSelectedItemIndex: (index) => setPlaylistSelectedItemIndex(index),
+  });
 
   // liveStore
   const setLiveItem = useSetAtom(atomLiveItem);
@@ -35,14 +47,6 @@ const PanelPlaylist = () => {
     setLiveItem(item);
     setLiveItemSelectedLineIndex(item?.content[0] ? 0 : -1);
   };
-
-  // playlistStore actions
-  const playlistShiftSelectedItemUp = useSetAtom(
-    atomPlaylistShiftSelectedItemUp,
-  );
-  const playlistShiftSelectedItemDown = useSetAtom(
-    atomPlaylistShiftSelectedItemDown,
-  );
 
   // Render playlist item
   const renderPlaylistItem = (item: PlaylistItem, index: number) => (
@@ -72,15 +76,45 @@ const PanelPlaylist = () => {
         />
       </BasePanelHeader>
 
-      <BaseList
-        className="overflow-y-auto"
-        items={playlistItems}
-        scrollToIndex={playlistSelectedItemIndex}
-        onKeyDownArrowUp={playlistShiftSelectedItemUp}
-        onKeyDownArrowDown={playlistShiftSelectedItemDown}
-        onKeyDownEnter={() => setLiveItemHandler(playlistSelectedItem)}
-        renderItem={renderPlaylistItem}
-      />
+      <div className="flex flex-1 divide-x divide-slate-300">
+        <BaseList
+          className="overflow-y-auto flex-1"
+          items={playlistItems}
+          scrollToIndex={playlistSelectedItemIndex}
+          onKeyDownArrowUp={listHandler.shiftSelectedItemUp}
+          onKeyDownArrowDown={listHandler.shiftSelectedItemDown}
+          onKeyDownEnter={() => setLiveItemHandler(playlistSelectedItem)}
+          renderItem={renderPlaylistItem}
+        />
+
+        <div className="flex flex-col gap-1 p-1">
+          <BaseButton
+            variant="default"
+            className="p-1"
+            onClick={listHandler.moveSelectedItemUp}
+            disabled={!listHandler.canShiftSelectedItemUp()}
+          >
+            <RiArrowUpLine className="w-4 h-4" />
+          </BaseButton>
+          <BaseButton
+            variant="default"
+            className="p-1"
+            onClick={listHandler.moveSelectedItemDown}
+            disabled={!listHandler.canShiftSelectedItemDown()}
+          >
+            <RiArrowDownLine className="w-4 h-4" />
+          </BaseButton>
+          <hr />
+          <BaseButton
+            variant="default"
+            className="p-1"
+            onClick={listHandler.removeSelectedItem}
+            disabled={!listHandler.selectedItem()}
+          >
+            <RiDeleteBin2Line className="w-4 h-4" />
+          </BaseButton>
+        </div>
+      </div>
     </BasePanel>
   );
 };

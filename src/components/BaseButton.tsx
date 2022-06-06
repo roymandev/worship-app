e801f982ce73b640c70eaa402bdc1a7eeb996e7b
Royman <royman.dev@gmail.com>
2022-06-07 05:41:48 +0700

    feat: BaseButton add primary variant

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -2,7 +2,9 @@ import { twMerge } from 'tailwind-merge';
 
 const buttonVariant = {
   default:
-    'rounded border border-slate-300 bg-slate-200 hover:bg-slate-300 hover:border-slate-400 disabled:bg-slate-100 disabled:border-slate-200 disabled:text-slate-400',
+    'border border-slate-300 bg-slate-200 hover:bg-slate-300 hover:border-slate-400 disabled:bg-slate-100 disabled:border-slate-200 disabled:text-slate-400',
+  primary:
+    'border text-blue-700 border-blue-300 bg-blue-200 hover:bg-blue-300 hover:border-blue-400 disabled:bg-blue-100 disabled:border-blue-200 disabled:text-blue-400',
 };
 
 export type BaseButtonProps = {
@@ -19,7 +21,11 @@ const BaseButton = ({
     <button
       type="button"
       {...rest}
-      className={twMerge(variant && buttonVariant[variant], className)}
+      className={twMerge(
+        'rounded',
+        variant && buttonVariant[variant],
+        className,
+      )}
     >
       {children}
     </button>

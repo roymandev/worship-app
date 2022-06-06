2c416c2434c03f32a08c4ff9ffb3e5dc8451b2d6
Royman <royman.dev@gmail.com>
2022-06-06 07:50:51 +0700

    feat: implement playlist export

--- src/components/PanelPlaylist.tsx
+++ src/components/PanelPlaylist.tsx
@@ -1,38 +1,17 @@
-import { RiCloseFill, RiUploadLine } from 'react-icons/ri';
-
 import BasePanel from './BasePanel';
-import BasePanelHeader from './BasePanelHeader';
-import BaseButton from './BaseButton';
 import { useState } from 'react';
 import PanelPlaylistList from './PanelPlaylistList';
 import PanelPlaylistImport from './PanelPlaylistImport';
-import { twMerge } from 'tailwind-merge';
+import PanelPlaylistHeader from './PanelPlaylistHeader';
+
+export type PanelPlaylistBody = 'list' | 'import' | 'export';
 
 const PanelPlaylist = () => {
-  const [panelBody, setPanelBody] = useState<'list' | 'import'>('list');
+  const [panelBody, setPanelBody] = useState<PanelPlaylistBody>('list');
 
   return (
     <BasePanel>
-      <BasePanelHeader>
-        <h2 className="px-2">Playlist</h2>
-        <BaseButton
-          variant="default"
-          className={twMerge(
-            'flex gap-1 items-center pr-3 pl-2 mx-1 ml-auto h-7',
-            panelBody === 'import' && 'border-slate-400 bg-slate-300',
-          )}
-          onClick={() =>
-            setPanelBody(panelBody === 'import' ? 'list' : 'import')
-          }
-        >
-          {panelBody === 'import' ? (
-            <RiCloseFill className="w-4 h-4" />
-          ) : (
-            <RiUploadLine className="w-4 h-4" />
-          )}
-          Import
-        </BaseButton>
-      </BasePanelHeader>
+      <PanelPlaylistHeader panelBody={panelBody} setPanelBody={setPanelBody} />
       {panelBody === 'list' && <PanelPlaylistList />}
       {panelBody === 'import' && (
         <PanelPlaylistImport close={() => setPanelBody('list')} />

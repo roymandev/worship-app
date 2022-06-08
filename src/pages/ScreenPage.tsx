ba1c1a3b2b54f52ec09b6ba023a8552e5440f52d
Royman <royman.dev@gmail.com>
2022-06-09 02:06:09 +0700

    feat: ScreenPage add window resize event

--- src/pages/ScreenPage.tsx
+++ src/pages/ScreenPage.tsx
@@ -1,12 +1,30 @@
-import { useAtomValue } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 import TextScreen from '../components/TextScreen';
 import { atomLiveItemSelectedLine } from '../stores/liveStore';
+import { atomUpdateScreenSettings } from '../stores/screenStore';
 
 const ScreenPage = () => {
   const liveItemSelectedLine = useAtomValue(atomLiveItemSelectedLine);
+  const setScreenSettings = useSetAtom(atomUpdateScreenSettings);
+
+  const resizeScreen = () => {
+    setScreenSettings({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    resizeScreen();
+
+    window.addEventListener('resize', resizeScreen);
+    return () => window.removeEventListener('resize', resizeScreen);
+  }, []);
+
   return (
     <div className="h-screen">
-      <TextScreen line={liveItemSelectedLine} mainScreen />
+      <TextScreen line={liveItemSelectedLine} />
     </div>
   );
 };

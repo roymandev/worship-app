e8871b6e8b8b809a974c76ea235057a72534f721
Royman <royman.dev@gmail.com>
2022-06-09 09:12:46 +0700

    feat!: clean project folder

--- src/stores/previewStore.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai';
-import { BaseItem } from '../types/playlistTypes';
-
-export const atomPreviewItem = atom<BaseItem | null>(null);
-export const atomPreviewItemContentSelectedLineIndex = atom(-1);

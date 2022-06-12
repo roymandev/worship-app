07b35511581f85322c59e9264f31083a61183c2c
Royman <royman.dev@gmail.com>
2022-06-12 09:24:43 +0700

    feat: add useOutsideClick hooks

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// Source Mantine.dev
+import { useEffect, useRef } from 'react';
+
+const DEFAULT_EVENTS = ['mousedown', 'touchstart'];
+
+export function useClickOutside<T extends HTMLElement = any>(
+  handler: () => void,
+  events?: string[] | null,
+  nodes?: HTMLElement[],
+) {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (Array.isArray(nodes)) {
+        const shouldIgnore = event?.target?.hasAttribute(
+          'data-ignore-outside-clicks',
+        );
+        const shouldTrigger = nodes.every(
+          (node) => !!node && !node.contains(event.target),
+        );
+        shouldTrigger && !shouldIgnore && handler();
+      } else if (ref.current && !ref.current.contains(event.target)) {
+        handler();
+      }
+    };
+
+    (events || DEFAULT_EVENTS).forEach((fn) =>
+      document.addEventListener(fn, listener),
+    );
+
+    return () => {
+      (events || DEFAULT_EVENTS).forEach((fn) =>
+        document.removeEventListener(fn, listener),
+      );
+    };
+  }, [ref, handler, nodes]);
+
+  return ref;
+}

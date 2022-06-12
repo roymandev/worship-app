4cb52cd9729833a08d7066478ab75b95b5287c35
Royman <royman.dev@gmail.com>
2022-06-12 12:50:10 +0700

    feat: BaseButton add red variant

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -5,6 +5,7 @@ const buttonVariant = {
     'text-slate-700 disabled:text-slate-400 bg-slate-200 hover:bg-slate-300 disabled:bg-slate-100 border border-slate-300 hover:border-slate-400 focus:border-slate-600 disabled:border-slate-200',
   primary:
     'text-blue-700 disabled:text-blue-400 bg-blue-200 hover:bg-blue-300 disabled:bg-blue-100 border border-blue-300 hover:border-blue-400 focus:border-blue-600 disabled:border-blue-200',
+  red: 'text-red-700 disabled:text-red-400 bg-red-200 hover:bg-red-300 disabled:bg-red-100 border border-red-300 hover:border-red-400 focus:border-red-600 disabled:border-red-200',
 };
 
 export type BaseButtonProps = {

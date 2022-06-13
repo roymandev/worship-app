74128e2198d1a6645ff1bdd7bb1b8d1b86225e57
Royman <royman.dev@gmail.com>
2022-06-13 07:51:57 +0700

    feat: ContextMenuPlaylistName add onClick handler

--- src/components/ContextMenu/ContextMenuPlaylistItem.tsx
+++ src/components/ContextMenu/ContextMenuPlaylistItem.tsx
@@ -1,20 +1,79 @@
+import { useAtomValue, useSetAtom } from 'jotai';
+import { atomContextMenuActive } from '../../stores/contextMenuStore';
+import {
+  atomLiveItem,
+  atomLiveItemContentSelectedLineIndex,
+} from '../../stores/liveStore';
+import {
+  atomPlaylistPanelContent,
+  atomPlaylistRemoveSelectedItem,
+  atomPlaylistSelectedItem,
+} from '../../stores/playlistStore';
 import BaseButton from '../BaseButton';
 
 const ContextMenuPlaylistItem = () => {
+  const setContextMenuActive = useSetAtom(atomContextMenuActive);
+  const playlistSelectedItem = useAtomValue(atomPlaylistSelectedItem);
+  const setLiveItem = useSetAtom(atomLiveItem);
+  const setLiveItemSelectedLineIndex = useSetAtom(
+    atomLiveItemContentSelectedLineIndex,
+  );
+  const setPlaylistPanelContent = useSetAtom(atomPlaylistPanelContent);
+  const setPlaylistRemoveSelectedItem = useSetAtom(
+    atomPlaylistRemoveSelectedItem,
+  );
+
+  const handleGoLive = () => {
+    setLiveItem(playlistSelectedItem);
+    setLiveItemSelectedLineIndex(playlistSelectedItem?.content[0] ? 0 : -1);
+    setContextMenuActive(null);
+  };
+
+  const handleEdit = () => {
+    setPlaylistPanelContent('itemEditor');
+    setContextMenuActive(null);
+  };
+
+  const hanldeRemove = () => {
+    setPlaylistRemoveSelectedItem();
+    setContextMenuActive(null);
+  };
+
+  const handleAdd = () => {
+    setPlaylistPanelContent('addItem');
+    setContextMenuActive(null);
+  };
+
   return (
     <>
-      <BaseButton variant="primary" className="py-1 text-left px-3">
+      <BaseButton
+        variant="primary"
+        className="py-1 text-left px-3"
+        onClick={handleGoLive}
+      >
         Go Live
       </BaseButton>
       <hr />
-      <BaseButton variant="default" className="py-1 text-left px-3">
+      <BaseButton
+        variant="default"
+        className="py-1 text-left px-3"
+        onClick={handleEdit}
+      >
         Edit
       </BaseButton>
-      <BaseButton variant="default" className="py-1 text-left px-3">
+      <BaseButton
+        variant="default"
+        className="py-1 text-left px-3"
+        onClick={hanldeRemove}
+      >
         Remove
       </BaseButton>
       <hr />
-      <BaseButton variant="red" className="py-1 text-left px-3">
+      <BaseButton
+        variant="red"
+        className="py-1 text-left px-3"
+        onClick={handleAdd}
+      >
         Add Item
       </BaseButton>
     </>

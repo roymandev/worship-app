3ea8057dc8dac92c9258a1f2fdf0e3b4fe118bfd
Royman <royman.dev@gmail.com>
2022-06-25 20:15:47 +0700

    refactor: move buttonColors styles to separate file

--- src/components/Buttons/BaseButtonText.tsx
+++ src/components/Buttons/BaseButtonText.tsx
@@ -1,17 +1,8 @@
 import { twMerge } from 'tailwind-merge';
-
-const buttonColor = {
-  gray: 'text-slate-700 hover:bg-slate-200 focus:bg-slate-400/30 disabled:text-slate-400 disabled:bg-slate-200/20',
-  blue: 'text-blue-700 hover:bg-blue-200 focus:bg-blue-400/30 disabled:text-blue-400 disabled:bg-blue-200/20',
-  red: 'text-red-700 hover:bg-red-200 focus:bg-red-400/30 disabled:text-red-400 disabled:bg-red-200/20',
-  green:
-    'text-green-700 hover:bg-green-200 focus:bg-green-400/30 disabled:text-green-400 disabled:bg-green-200/20',
-  purple:
-    'text-purple-700 hover:bg-purple-200 focus:bg-purple-400/30 disabled:text-purple-400 disabled:bg-purple-200/20',
-};
+import { ButtonColors, BUTTON_TEXT_COLORS } from './buttonColor';
 
 export type BaseButtonTextProps = {
-  color: keyof typeof buttonColor;
+  color: keyof ButtonColors;
 } & React.ComponentPropsWithoutRef<'button'>;
 
 const BaseButtonText = ({ color, className, ...rest }: BaseButtonTextProps) => {
@@ -20,7 +11,7 @@ const BaseButtonText = ({ color, className, ...rest }: BaseButtonTextProps) => {
       type="button"
       className={twMerge(
         'outline-none',
-        color && buttonColor[color],
+        color && BUTTON_TEXT_COLORS[color],
         className,
       )}
       {...rest}

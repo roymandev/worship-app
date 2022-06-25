48fa3b38a577c4f98d9dc6d7cdd09706dae80d3a
Royman <royman.dev@gmail.com>
2022-06-26 02:45:16 +0700

    feat: BasePanelHeader add className props

--- src/components/BasePanelHeader.tsx
+++ src/components/BasePanelHeader.tsx
@@ -2,15 +2,21 @@ import { twMerge } from 'tailwind-merge';
 
 interface BasePanelHeaderProps {
   children: React.ReactNode;
+  className?: string;
   sub?: boolean;
 }
 
-const BasePanelHeader = ({ children, sub = false }: BasePanelHeaderProps) => {
+const BasePanelHeader = ({
+  children,
+  className,
+  sub = false,
+}: BasePanelHeaderProps) => {
   return (
     <div
       className={twMerge(
         'flex items-center border-b border-slate-300 flex-shrink-0',
         sub ? 'h-9 bg-slate-100' : 'h-9',
+        className,
       )}
     >
       {children}

bcd30f7d335ffd9cf74538cf6de4480a4d821ced
Royman <royman.dev@gmail.com>
2022-07-07 03:32:34 +0700

    feat: ContextMenuDatabase add handler
    Go live and Add to playlist

--- src/components/ContextMenu/ContextMenuDatabaseItem.tsx
+++ src/components/ContextMenu/ContextMenuDatabaseItem.tsx
@@ -1,10 +1,49 @@
-import { RiPlayListAddFill } from 'react-icons/ri';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { RiPlayFill, RiPlayListAddFill } from 'react-icons/ri';
+import { atomContextMenuActive } from '../../stores/contextMenuStore';
+import { atomDatabaseParsedSelectedItem } from '../../stores/databaseStore';
+import {
+  atomLiveItem,
+  atomLiveItemContentSelectedLineIndex,
+} from '../../stores/liveStore';
+import { atomPlaylistAddItem } from '../../stores/playlistStore';
 import ButtonContextMenu from '../Buttons/ButtonContextMenu';
 
 const ContextMenuDatabaseItem = () => {
+  const setActive = useSetAtom(atomContextMenuActive);
+  const databaseParsedSelectedItem = useAtomValue(
+    atomDatabaseParsedSelectedItem,
+  );
+
+  // Add to playlist handler
+  const setPlaylistAddItem = useSetAtom(atomPlaylistAddItem);
+  const handleAddToPlaylist = () => {
+    databaseParsedSelectedItem &&
+      setPlaylistAddItem(databaseParsedSelectedItem);
+    setActive(null);
+  };
+
+  // Go live hanlder
+  const setLiveItem = useSetAtom(atomLiveItem);
+  const setLiveItemContentSelectedLineIndex = useSetAtom(
+    atomLiveItemContentSelectedLineIndex,
+  );
+  const handleGoLive = () => {
+    setLiveItem(databaseParsedSelectedItem);
+    setLiveItemContentSelectedLineIndex(
+      databaseParsedSelectedItem?.content[0] ? 0 : -1,
+    );
+    setActive(null);
+  };
+
   return (
     <>
-      <ButtonContextMenu color="blue">
+      <ButtonContextMenu color="blue" onClick={handleGoLive}>
+        Go Live
+        <RiPlayFill className="h-4 w-4" />
+      </ButtonContextMenu>
+      <hr />
+      <ButtonContextMenu color="gray" onClick={handleAddToPlaylist}>
         Add to playlist
         <RiPlayListAddFill className="h-4 w-4" />
       </ButtonContextMenu>

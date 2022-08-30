05d24e5cad1089aa00f259b11a81809d1251ebd3
Royman <royman.dev@gmail.com>
2022-08-31 06:40:59 +0700

    feat: PanelPlaylist add content
    content export and import

---
+++ src/components/PanelPlaylist/ContentExport.tsx
@@ -0,0 +1,43 @@
+import BasePanelHeader from '@/components/BasePanelHeader';
+import ButtonPrimary from '@/components/Buttons/ButtonPrimary';
+import usePlaylist from '@/components/hooks/usePlaylist';
+import { atomPlaylistPanelContent } from '@/stores/playlistStore';
+import { useSetAtom } from 'jotai';
+
+const ContentExport = () => {
+  const { name, download } = usePlaylist();
+  const setPanelContent = useSetAtom(atomPlaylistPanelContent);
+
+  const downloadHandler = () => {
+    download();
+    setPanelContent('list');
+  };
+
+  return (
+    <>
+      <BasePanelHeader sub>
+        <h3 className="px-1">Export</h3>
+      </BasePanelHeader>
+
+      <div className="space-y-3 p-3">
+        <p>
+          Download &quot;<b>{name}.WORSHIP</b>&quot; playlist.
+        </p>
+
+        <ButtonPrimary color="blue" onClick={downloadHandler}>
+          Download
+        </ButtonPrimary>
+
+        <ButtonPrimary
+          color="gray"
+          className="ml-1"
+          onClick={() => setPanelContent('list')}
+        >
+          Cancel
+        </ButtonPrimary>
+      </div>
+    </>
+  );
+};
+
+export default ContentExport;
--- src/components/PanelPlaylist/index.tsx
+++ src/components/PanelPlaylist/index.tsx
@@ -1,27 +1,18 @@
 import BasePanel from '@/components/BasePanel';
-import BasePanelHeader from '@/components/BasePanelHeader';
-import ButtonPrimary from '@/components/Buttons/ButtonPrimary';
-import { RiDownloadLine, RiUploadLine } from 'react-icons/ri';
+import ContentExport from '@/components/PanelPlaylist/ContentExport';
+import ContentImport from '@/components/PanelPlaylist/ContentImport';
+import Header from '@/components/PanelPlaylist/Header';
+import { atomPlaylistPanelContent } from '@/stores/playlistStore';
+import { useAtomValue } from 'jotai';
 
 const PanelPlaylist = () => {
+  const panelContent = useAtomValue(atomPlaylistPanelContent);
+
   return (
     <BasePanel>
-      <BasePanelHeader>
-        <h2 className="px-1">Playlist</h2>
-
-        <ButtonPrimary
-          color="gray"
-          className="ml-auto h-full py-0"
-          withIcon="left"
-        >
-          <RiUploadLine className="h-4 w-4" />
-          Import
-        </ButtonPrimary>
-        <ButtonPrimary color="gray" className="h-full py-0" withIcon="left">
-          <RiDownloadLine className="h-4 w-4" />
-          Export
-        </ButtonPrimary>
-      </BasePanelHeader>
+      <Header />
+      {panelContent === 'export' && <ContentExport />}
+      {panelContent === 'import' && <ContentImport />}
     </BasePanel>
   );
 };

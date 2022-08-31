6f486f6a78c89721c5554a5a2598c05e4aa2464a
Royman <royman.dev@gmail.com>
2022-09-01 06:26:57 +0700

    feat: usePlaylist add deleteSelectedItem method

--- src/components/hooks/usePlaylist.ts
+++ src/components/hooks/usePlaylist.ts
@@ -15,6 +15,12 @@ const usePlaylist = () => {
     atomPlaylistSelectedItemId,
   );
 
+  const getSelectedItemIndex = () =>
+    items.findIndex((item) => item.id === selectedItemId);
+
+  const canShiftSelectedItemUp = () => items[getSelectedItemIndex() - 1];
+  const canShiftSelectedItemDown = () => items[getSelectedItemIndex() + 1];
+
   const upload = (playlist: PlaylistFile) => {
     setName(playlist.name);
     setItems(playlist.items || []);
@@ -23,6 +29,22 @@ const usePlaylist = () => {
   const download = () =>
     downloadObject({ items }, (name || 'Untitled') + FILE_EXT);
 
+  const shiftSelectedItemUp = () =>
+    setSelectedItemId(items[getSelectedItemIndex() - 1].id);
+
+  const shiftSelectedItemDown = () =>
+    setSelectedItemId(items[getSelectedItemIndex() + 1].id);
+
+  const deleteSelectedItem = () => {
+    if (canShiftSelectedItemUp()) shiftSelectedItemUp();
+    else if (canShiftSelectedItemDown()) shiftSelectedItemDown();
+    else setSelectedItemId(null);
+
+    setItems((prevItems) =>
+      prevItems.filter((item) => item.id !== selectedItemId),
+    );
+  };
+
   return {
     name,
     setName,
@@ -30,8 +52,13 @@ const usePlaylist = () => {
     setItems,
     selectedItemId,
     setSelectedItemId,
+    canShiftSelectedItemUp,
+    canShiftSelectedItemDown,
     upload,
     download,
+    deleteSelectedItem,
+    shiftSelectedItemUp,
+    shiftSelectedItemDown,
   };
 };
 

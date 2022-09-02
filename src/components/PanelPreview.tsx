06ddfcdf4ad7d137bd6785e9b2f5fc2a1ef7a06a
Royman <royman.dev@gmail.com>
2022-09-03 01:35:29 +0700

    feat: PanelPreview add Screen

--- src/components/PanelPreview.tsx
+++ src/components/PanelPreview.tsx
@@ -2,6 +2,7 @@ import BaseList from '@/components/BaseList';
 import BasePanel from '@/components/BasePanel';
 import BasePanelHeader from '@/components/BasePanelHeader';
 import ItemContentLine from '@/components/ItemContentLine';
+import Screen, { ScreenRef } from '@/components/Screen';
 import { atomPlaylistSelectedItem } from '@/stores/playlistStore';
 import {
   atomPreviewItem,
@@ -9,24 +10,33 @@ import {
   atomPreviewItemContentSelectedLineIndex,
 } from '@/stores/previewStore';
 import { useAtom, useAtomValue } from 'jotai';
-import { useEffect } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
 import Split from 'react-split';
 
-const PanelPreview = () => {
+const PanelPreview = forwardRef<ScreenRef>((props, ref) => {
   const [item, setItem] = useAtom(atomPreviewItem);
   const [contentSelectedLineIndex, setContentSelectedLineIndex] = useAtom(
     atomPreviewItemContentSelectedLineIndex,
   );
   const selectedPlaylistItem = useAtomValue(atomPlaylistSelectedItem);
   const selectedLine = useAtomValue(atomPreviewItemContentSelectedLine);
+  const screenRef = useRef<ScreenRef | null>(null);
 
   useEffect(() => {
     setItem(selectedPlaylistItem);
     setContentSelectedLineIndex(selectedPlaylistItem?.content[0] ? 0 : -1);
   }, [selectedPlaylistItem]);
 
+  useImperativeHandle(ref, () => ({
+    resizeScreen: () => screenRef.current?.resizeScreen(),
+  }));
+
   return (
-    <Split direction="vertical" gutterSize={4}>
+    <Split
+      direction="vertical"
+      gutterSize={4}
+      onDrag={() => screenRef.current?.resizeScreen()}
+    >
       <BasePanel>
         <BasePanelHeader>
           <h2 className="px-1">Preview</h2>
@@ -54,9 +64,13 @@ const PanelPreview = () => {
         )}
       </BasePanel>
 
-      <BasePanel>{selectedLine?.text}</BasePanel>
+      <BasePanel>
+        <Screen ref={screenRef} line={selectedLine} />
+      </BasePanel>
     </Split>
   );
-};
+});
+
+PanelPreview.displayName = 'PanelPreview';
 
 export default PanelPreview;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,25 @@
 import BasePanel from '@/components/BasePanel';
 import PanelPlaylist from '@/components/PanelPlaylist';
 import PanelPreview from '@/components/PanelPreview';
+import { ScreenRef } from '@/components/Screen';
+import { useRef } from 'react';
 import Split from 'react-split';
 
 const Home = () => {
+  const panelPreviewScreenRef = useRef<ScreenRef | null>(null);
+
+  const dragHandler = () => {
+    panelPreviewScreenRef.current?.resizeScreen();
+  };
+
   return (
     <Split
       className="fixed inset-0 flex bg-slate-300 p-1 text-sm text-slate-700"
       gutterSize={4}
+      onDrag={dragHandler}
     >
       <PanelPlaylist />
-      <PanelPreview />
+      <PanelPreview ref={panelPreviewScreenRef} />
       <BasePanel>Right</BasePanel>
     </Split>
   );

2ca87133611367bffee45b14b6928cfe437e1880
Royman <royman.dev@gmail.com>
2022-09-02 21:43:50 +0700

    feat: add routes loading fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import Loading from '@/components/Fallback/Loading';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from '@/pages/Home';
+
+const Home = lazy(() => import('@/pages/Home'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Suspense
+      fallback={
+        <div className="fixed inset-0 flex">
+          <Loading />
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Suspense>
   );
 }
 

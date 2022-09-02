cbac88c4fee258dcbb9f47c36f1c6bf9603eb6f5
Royman <royman.dev@gmail.com>
2022-09-02 22:06:50 +0700

    chore: reduce red button contrast when disabled

--- src/components/Buttons/buttonColors.ts
+++ src/components/Buttons/buttonColors.ts
@@ -3,7 +3,7 @@ export type ButtonColors = typeof BUTTON_TEXT_COLORS;
 export const BUTTON_TEXT_COLORS = {
   gray: 'text-slate-700 hover:bg-slate-200 focus:bg-slate-400/30 disabled:text-slate-400 disabled:bg-slate-200/20',
   blue: 'text-blue-700 hover:bg-blue-200 focus:bg-blue-400/30 disabled:text-blue-400 disabled:bg-blue-200/20',
-  red: 'text-red-700 hover:bg-red-200 focus:bg-red-400/30 disabled:text-red-400 disabled:bg-red-200/20',
+  red: 'text-red-700 hover:bg-red-200 focus:bg-red-400/30 disabled:text-red-300 disabled:bg-red-200/20',
   green:
     'text-green-700 hover:bg-green-200 focus:bg-green-400/30 disabled:text-green-400 disabled:bg-green-200/20',
   purple:
@@ -15,7 +15,7 @@ export const BUTTON_TEXT_COLORS = {
 export const BUTTON_COLORS: ButtonColors = {
   gray: 'bg-slate-200/30 border-slate-300 hover:border-slate-500 focus:border-slate-500 disabled:border-slate-200',
   blue: 'bg-blue-200/30 border-blue-300 hover:border-blue-500 focus:border-blue-500 disabled:border-blue-200',
-  red: 'bg-red-200/30 border-red-300 hover:border-red-500 focus:border-red-500 disabled:border-red-200',
+  red: 'bg-red-200/30 border-red-300 hover:border-red-500 focus:border-red-500 disabled:border-red-100',
   green:
     'bg-green-200/30 border-green-300 hover:border-green-500 focus:border-green-500 disabled:border-green-200',
   purple:

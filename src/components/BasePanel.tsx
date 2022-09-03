e02d28959c94ae2e325763ce65a1a50433cdd284
Royman <royman.dev@gmail.com>
2022-09-04 00:23:22 +0700

    chore: BasePanel add className props

--- src/components/BasePanel.tsx
+++ src/components/BasePanel.tsx
@@ -1,9 +1,17 @@
+import { twclsx } from '@/lib/twclsx';
+
 export interface BasePanelProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const BasePanel = ({ children }: BasePanelProps) => (
-  <div className="flex flex-1 flex-col overflow-hidden rounded bg-white shadow">
+const BasePanel = ({ children, className }: BasePanelProps) => (
+  <div
+    className={twclsx(
+      'flex flex-1 flex-col overflow-hidden rounded bg-white shadow',
+      className,
+    )}
+  >
     {children}
   </div>
 );

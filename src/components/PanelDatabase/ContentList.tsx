fe9b59605d9ba91055b2d01765531015b8b1ce83
Royman <royman.dev@gmail.com>
2022-09-06 02:51:55 +0700

    feat: PanelDatabase ContentList add list actions

--- src/components/PanelDatabase/ContentList.tsx
+++ src/components/PanelDatabase/ContentList.tsx
@@ -2,15 +2,19 @@ import BaseInput from '@/components/BaseInput';
 import BaseList from '@/components/BaseList';
 import BaseListItem from '@/components/BaseListItem';
 import BasePanelHeader from '@/components/BasePanelHeader';
+import ButtonPrimary from '@/components/Buttons/ButtonPrimary';
 import { atomSongs } from '@/stores/databaseStore';
+import { atomPlaylistItems } from '@/stores/playlistStore';
 import {
   atomPreviewItem,
   atomPreviewItemContentSelectedLineIndex,
 } from '@/stores/previewStore';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { useState } from 'react';
+import { RiPlayListAddFill } from 'react-icons/ri';
 
 const ContentList = () => {
+  const setPlaylistItems = useSetAtom(atomPlaylistItems);
   const setPreviewItem = useSetAtom(atomPreviewItem);
   const setPreviewItemContentSelectedLineIndex = useSetAtom(
     atomPreviewItemContentSelectedLineIndex,
@@ -28,6 +32,13 @@ const ContentList = () => {
     setPreviewItemContentSelectedLineIndex(item?.content[0] ? 0 : -1);
   };
 
+  const addToPlaylistHandler = () => {
+    const item = songs[selectedItemIndex] || null;
+    if (item) {
+      setPlaylistItems((prevItems) => [...prevItems, item]);
+    }
+  };
+
   return (
     <>
       <BasePanelHeader sub>
@@ -38,21 +49,35 @@ const ContentList = () => {
           placeholder="Search"
         />
       </BasePanelHeader>
-      <BaseList
-        items={songs}
-        selectedItemIndex={selectedItemIndex}
-        onSelectItem={onSelectItemHandler}
-        renderItem={(song, isSelected, index) => (
-          <BaseListItem
-            key={song.id}
-            className="py-1 px-2"
-            isSelected={isSelected}
-            onClick={() => onSelectItemHandler(index)}
+
+      <div className="flex flex-1 divide-x divide-slate-300">
+        <BaseList
+          items={songs}
+          selectedItemIndex={selectedItemIndex}
+          onSelectItem={onSelectItemHandler}
+          renderItem={(song, isSelected, index) => (
+            <BaseListItem
+              key={song.id}
+              className="py-1 px-2"
+              isSelected={isSelected}
+              onClick={() => onSelectItemHandler(index)}
+            >
+              {song.title}
+            </BaseListItem>
+          )}
+        />
+
+        <div className="flex flex-col gap-1 p-1">
+          <ButtonPrimary
+            tabIndex={-1}
+            color="blue"
+            className="p-1.5"
+            onClick={addToPlaylistHandler}
           >
-            {song.title}
-          </BaseListItem>
-        )}
-      />
+            <RiPlayListAddFill className="h-4 w-4" />
+          </ButtonPrimary>
+        </div>
+      </div>
     </>
   );
 };

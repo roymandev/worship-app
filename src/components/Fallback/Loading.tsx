d62ebfa86ec2b996073c385a879f40b4ce9fbdfa
Royman <royman.dev@gmail.com>
2022-09-19 07:03:04 +0700

    chore: fallback loading remove white background

--- src/components/Fallback/Loading.tsx
+++ src/components/Fallback/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => (
-  <div className="grid flex-1 place-items-center bg-white">
-    <img src="/Worship%20App.svg" alt="My Notes Logo" className="h-20 w-20 " />
+  <div className="grid flex-1 place-items-center">
+    <img src="/Worship%20App.svg" alt="My Notes Logo" className="h-20 w-20" />
   </div>
 );
 
--- src/components/Fallback/LoadingSpinner.tsx
+++ src/components/Fallback/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import { RiLoader4Fill } from 'react-icons/ri';
 
 const LoadingSpinner = () => (
-  <div className="grid flex-1 place-items-center bg-white">
+  <div className="grid flex-1 place-items-center">
     <RiLoader4Fill className="h-20 w-20 flex-1 animate-spin" />
   </div>
 );

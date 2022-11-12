edf4eddb25f100004551ad1df84d1f4e4a2a6b70
Royman <royman.dev@gmail.com>
2022-11-12 22:05:02 +0700

    feat: set ButtonPrimary height and padding

--- src/components/Buttons/ButtonPrimary.tsx
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -14,8 +14,8 @@ const ButtonPrimary = ({
   return (
     <BaseButton
       className={twclsx(
-        'py-1',
-        withIcon ? 'flex items-center gap-1' : 'px-3',
+        'py-1 h-8',
+        withIcon ? 'flex items-center gap-1' : 'px-4',
         withIcon === 'left' && 'pr-3 pl-2',
         withIcon === 'right' && 'pr-2 pl-3',
         className,

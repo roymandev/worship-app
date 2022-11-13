a6d48d034a39f2dda5b59858c158d791fe9a4225
Royman <royman.dev@gmail.com>
2022-11-13 21:02:44 +0700

    feat: make loading fallback dark

--- src/components/Fallback/LoadingSpinner.tsx
+++ src/components/Fallback/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { RiLoader4Fill } from 'react-icons/ri';
 
 const LoadingSpinner = () => (
   <div className="grid flex-1 place-items-center">
-    <RiLoader4Fill className="h-20 w-20 flex-1 animate-spin" />
+    <RiLoader4Fill className="h-20 w-20 flex-1 animate-spin text-zinc-600" />
   </div>
 );
 

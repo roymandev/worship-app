729efe3a5ab53fb4690552ad33b462fa0ca38fd6
Royman <royman.dev@gmail.com>
2022-11-15 22:29:10 +0700

    feat: use line instead of border in BasePanelHeader

--- src/components/BasePanelHeader.tsx
+++ src/components/BasePanelHeader.tsx
@@ -12,15 +12,18 @@ const BasePanelHeader = ({
   sub = false,
 }: BasePanelHeaderProps) => {
   return (
-    <div
-      className={twclsx(
-        'flex items-center border-b border-zinc-600 flex-shrink-0 p-1 gap-1 h-9',
-        sub && 'bg-zinc-900',
-        className,
-      )}
-    >
-      {children}
-    </div>
+    <>
+      <div
+        className={twclsx(
+          'flex items-center flex-shrink-0 p-1 gap-1 h-9',
+          sub && 'bg-zinc-900',
+          className,
+        )}
+      >
+        {children}
+      </div>
+      <hr className="border-zinc-600" />
+    </>
   );
 };
 

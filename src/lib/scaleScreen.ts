863ea32721401535068c0fb7d0f00fd85e055385
Royman <royman.dev@gmail.com>
2023-01-15 16:57:56 +0700

    refactor(lib): scaleScreen

--- src/lib/scaleScreen.ts
+++ src/lib/scaleScreen.ts
@@ -6,6 +6,13 @@ interface Container {
 type ScreenSize = Record<string, number>;
 
 export const scaleScreen = (screenSize: ScreenSize, container: Container) => {
+  const zoom = calculateZoom(screenSize, container);
+  const scaledSize = scaleScreenSize(screenSize, zoom);
+  const scaledStyle = createScaledStyle(scaledSize);
+  return { scaledSize, scaledStyle };
+};
+
+const calculateZoom = (screenSize: ScreenSize, container: Container) => {
   // Set zoom based on container width
   let zoom = (container.width / screenSize.width) * 100;
 
@@ -13,15 +20,23 @@ export const scaleScreen = (screenSize: ScreenSize, container: Container) => {
   if ((screenSize.height / 100) * zoom > container.height)
     zoom = (container.height / screenSize.height) * 100;
 
-  const scaledSize = {} as ScreenSize;
-  const scaledStyle = {} as Record<string, string>;
+  return zoom;
+};
 
-  // Scale all screenSize property
+const scaleScreenSize = (screenSize: ScreenSize, zoom: number) => {
+  const scaledSize = {} as ScreenSize;
   let key: keyof ScreenSize;
   for (key in screenSize) {
     scaledSize[key] = (screenSize[key] / 100) * zoom;
+  }
+  return scaledSize;
+};
+
+const createScaledStyle = (scaledSize: ScreenSize) => {
+  const scaledStyle = {} as Record<string, string>;
+  let key: keyof ScreenSize;
+  for (key in scaledSize) {
     scaledStyle[key] = scaledSize[key] + 'px';
   }
-
-  return { scaledSize, scaledStyle };
+  return scaledStyle;
 };

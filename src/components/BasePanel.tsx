c5d93fbc689f0526641b142ebf5332ceae5f2c23
Royman <royman.dev@gmail.com>
2023-01-17 07:34:13 +0700

    chore: remove BasePanel rounded class

--- src/components/BasePanel.tsx
+++ src/components/BasePanel.tsx
@@ -6,12 +6,7 @@ export interface BasePanelProps {
 }
 
 const BasePanel = ({ children, className }: BasePanelProps) => (
-  <div
-    className={twclsx(
-      'flex flex-1 flex-col overflow-hidden rounded',
-      className,
-    )}
-  >
+  <div className={twclsx('flex flex-1 flex-col overflow-hidden', className)}>
     {children}
   </div>
 );

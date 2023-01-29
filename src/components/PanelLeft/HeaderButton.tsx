cd99d7241aa58a7cf7997ffd4e3f405a0ad51bb2
Royman <royman.dev@gmail.com>
2023-01-29 12:59:42 +0700

    chore: change PanelLeft header button color

--- src/components/PanelLeft/HeaderButton.tsx
+++ src/components/PanelLeft/HeaderButton.tsx
@@ -16,7 +16,7 @@ export const HeaderButton = ({
       className={twclsx(
         '-mb-px flex items-center gap-3 border-b border-zinc-600 px-3',
         active
-          ? 'border-sky-500 bg-zinc-600/50 text-white'
+          ? 'border-sky-600 bg-sky-700/50 text-white'
           : 'hover:bg-zinc-700/50',
       )}
       onClick={onClick}

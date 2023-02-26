b3e0f1342169e00c9ac74b379fce0cf798941ea8
Royman <royman.dev@gmail.com>
2023-02-27 04:07:07 +0700

    refactor: loading fullscreen

--- src/components/Fallback/LoadingFullscreen.tsx
+++ src/components/Fallback/LoadingFullscreen.tsx
@@ -1,9 +1,14 @@
-import Loading from '@/components/Fallback/Loading';
+import { Grid, Image } from '@mantine/core';
 
 const LoadingFullscreen = () => (
-  <div className="fixed inset-0 flex bg-zinc-800">
-    <Loading />
-  </div>
+  <Grid justify="center" align="center" sx={{ position: 'fixed', inset: 0 }}>
+    <Image
+      src="/Worship%20App.svg"
+      alt="My Notes Logo"
+      width={100}
+      height={100}
+    />
+  </Grid>
 );
 
 export default LoadingFullscreen;
